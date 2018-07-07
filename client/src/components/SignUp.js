46525920b0ef1ca06075ad658ad6c594ab074534
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-07-07 17:35:29 -0500

    Makes it so users can now sign up through Firebase at the /signup path

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,12 +1,118 @@
 import React, { Component } from 'react';
+import { Link,  withRouter, } from 'react-router-dom';
+import * as routes from '../constants/routes';
+import { auth } from '../firebase';
 
-export default class SignUp extends Component {
+const SignUpPage = ({ history }) =>
+  <div>
+    <h1>SignUp</h1>
+    <SignUpForm history={history} />
+  </div>
+
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value,
+});
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = (event) => {
+    const {
+      username,
+      email,
+      passwordOne,
+    } = this.state;
+
+    const {
+      history,
+    } = this.props;
+
+    auth.doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState(() => ({ ...INITIAL_STATE }));
+        history.push(routes.HOME);
+      })
+      .catch(error => {
+        this.setState(byPropKey('error', error));
+      });
+
+    event.preventDefault();
+  }
+
   render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === '';
+
     return (
-      <div className="signup">
-      <h1>Sign Up page</h1>
-        {/* { this.props.children } */}
-      </div>
-    )
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={event => this.setState(byPropKey('username', event.target.value))}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          value={email}
+          onChange={event => this.setState(byPropKey('email', event.target.value))}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={passwordOne}
+          onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          value={passwordTwo}
+          onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
   }
 }
+
+const SignUpLink = () =>
+  <p>
+    Don't have an account?
+    {' '}
+    <Link to={routes.SIGN_UP}>Sign Up</Link>
+  </p>
+
+// "Just in case: There is no particular reason why I wrapped the SignUpPage and not the SignUpForm with the higher order component."
+export default withRouter(SignUpPage);
+
+export {
+  SignUpForm,
+  SignUpLink,
+};
