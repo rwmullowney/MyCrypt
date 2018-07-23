b7150d6d49617e63dd8115a429617256525cab8a
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-07-22 22:54:06 -0500

    Updates the landing page text and formatting

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 
+var textStyle = {
+  maxWidth: "50%"
+}
+
 export default class Home extends Component {
   render() {
     return (
-      <div className="container mt-5 text-center">
-        {/* TODO: Why doesn't the font-weight work? */}
-        <div>
-          <br />
-          <h2 className="font-weight-light">Hey there!  Welcome to MyCrypto - we're your digital wallet and market monitor.</h2>
-          <br />
-          {/* <hr style={{width: "10%"}}/> */}
-          <h4 className="font-weight-light">Interested in cryptocurrency but don't want to risk gambling with real money?</h4>
-          <br />
-          <h4 className="font-weight-light">Try this site risk free! Invest with "$10,000" and select from the top 10 list of crypto stocks.</h4>
-          <br />
-          <h4 className="font-weight-light">Click Transactions and begin investing now!</h4>
-        </div>
+      <div className="container mt-4 text-center" style={textStyle}>
+        <br />
+        <h2 className="font-weight-light">
+          Hey there!  Welcome to MyCrypto.
+
+        </h2>
+        <h3 className="font-weight-light">We're your digital wallet and market monitor.</h3>
+        <br />
+        {/* <hr style={{width: "10%"}}/> */}
+        <h4 className="font-weight-light">If you're interested in cryptocurrency,
+          but you're not quite comfortable spending your hard earned cash on it yet, you've come to the right place.</h4>
+        <br />
+        <h4 className="font-weight-light">With MyCrypto, you can play the cryptocurrency market with virtual money, risk free.</h4>
+        <br />
+        <h4 className="font-weight-light">Sign up now and get $10,000 to experiment with buying and selling from the top 10 list of cryptocurrencies on the market.</h4>
       </div>
     )
   }
