e1673ede09d504948090f224da85e8932925027b
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-07-24 13:00:36 -0500

    Updates text on landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -16,9 +16,9 @@ export default class Home extends Component {
     return (
       <div className="container mt-4 text-center" style={textStyle}>
         <div className="rounded" style={containerStyle}>
-          <br />
+        <br />
           <h2 className="mb-4 font-weight-light">Hey there!  Welcome to MyCrypto.</h2>
-          <h3 className="font-weight-light" >We're your digital wallet and market monitor.</h3>
+          <h3 className="font-weight-light" >We're your digital wallet and cryptocurrency market simulator.</h3>
           <br />
           {/* <hr style={{width: "10%"}}/> */}
           <h4 className="font-weight-light" >If you're interested in cryptocurrency,
@@ -26,7 +26,7 @@ export default class Home extends Component {
           <br />
           <h4 className="font-weight-light" >With MyCrypto, you can play the cryptocurrency market with virtual money, risk free.</h4>
           <br />
-          <h4 className="font-weight-light" >Sign up now and get $10,000 to experiment with buying and selling from the top 10 list of cryptocurrencies on the market.</h4>
+          <h4 className="font-weight-light" >Sign up now and get $10,000 to experiment with buying and selling from the 10 most popular cryptocurrencies on the market.</h4>
         </div>
       </div>
     )
