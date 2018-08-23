089c0f8915115b2401fec56f2960c9c1a3e6d98a
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-08-22 23:36:57 -0500

    updates port info for cloud server

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 1337;
+require('dotenv').config();
 
 // Define middleware here
+app.set('port', (process.env.PORT || 1337));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
@@ -16,7 +18,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cryptotransactions");
+mongoose.connect(process.env.MONGODB_URI || 
+  // "mongodb://localhost/cryptotransactions"
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`
+);
 
 // Start the API server
 app.listen(PORT, function() {
