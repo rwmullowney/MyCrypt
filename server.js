617efd464fdb758e052a86afaebd3504e916484a
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-08-28 12:28:01 -0500

    Minor changes to server to get connected to mLab

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cryptotransactions");
+mongoose.connect(process.env.MONGODB_URI);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(rocess.env.PORT || 3001, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
